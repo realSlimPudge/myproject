9ccb890d20cdaa94e070e04faf8cb87533e75835
realSlimPudge <komissarov010806@gmail.com>
2024-09-13 16:35:10 +0300

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useState } from 'react'
+import Square from './components/Square'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let props = 'props'
+
+	function Mycounter() {
+		const [count, setCount] = useState(0)
+		function plus() {
+			setCount(count + 1)
+		}
+		function minus() {
+			setCount(count - 1)
+		}
+		return (
+			<div>
+				<div>{count}</div>
+				<button onClick={plus}>+</button>
+				<button onClick={minus}>-</button>
+			</div>
+		)
+	}
+	function buttonClick() {
+		console.log('button has been clicked')
+	}
+	return (
+		<div className='counter'>
+			<button onClick={buttonClick}>Click</button>
+			<Square valueProps={props}></Square>
+			<Square></Square>
+			<Square></Square>
+			<Square></Square>
+			<Mycounter></Mycounter>
+			<Mycounter></Mycounter>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/Square.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Square(props) {
+	return (
+		<div>
+			{props.valueProps}
+			<h3>Hello World!</h3>
+		</div>
+	)
+}
